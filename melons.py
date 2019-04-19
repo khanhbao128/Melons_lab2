795b912efd2b2fa799f235e225fcf4223bcc428a
Hackbright Student <no-reply@hackbrightacademy.com>
2019-04-18 17:54:14 -0700

    melon lab 2 completed

--- melons.py
+++ melons.py
@@ -2,17 +2,26 @@
 
 class AbstractMelonOrder():
 
-    def __init__(self, species, qty):
+    def __init__(self, species, qty, country_code=None):
 
         self.species = species
         self.qty = qty
         self.shipped = False
+        if country_code:
+            self.country_code = country_code
     
     def get_total(self):
         """Calculate price, including tax."""
 
         base_price = 5
+
+        if self.species == "christmas":
+            base_price = base_price * 1.5
+
         total = (1 + self.tax) * self.qty * base_price
+
+        if self.order_type == "international":
+            total += 3
 
         return total
 
@@ -24,11 +33,12 @@ class AbstractMelonOrder():
 
 class DomesticMelonOrder(AbstractMelonOrder):
     """A melon order within the USA."""
+    order_type = "domestic"
 
     def __init__(self, species, qty):
         """Initialize melon order attributes."""
         super().__init__(species, qty)
-        self.order_type = "domestic"
+        
         # self.species = species
         # self.qty = qty
         # self.shipped = False
@@ -39,12 +49,11 @@ class DomesticMelonOrder(AbstractMelonOrder):
 
 class InternationalMelonOrder(AbstractMelonOrder):
     """An international (non-US) melon order."""
+    order_type = "international"
 
     def __init__(self, species, qty, country_code):
         """Initialize melon order attributes."""
-        super().__init__(species, qty)#, country_code)
-        self.country_code = country_code
-        self.order_type = "international"
+        super().__init__(species, qty, country_code)#, country_code)
         # self.species = species
         # self.qty = qty
         # self.shipped = False
@@ -58,3 +67,20 @@ class InternationalMelonOrder(AbstractMelonOrder):
         """Return the country code."""
 
         return self.country_code
+
+
+class GovernmentMelonOrder(AbstractMelonOrder):
+
+    passed_inspection = False
+
+
+    def __init__(self, species, qty):
+        super().__init__(species, qty)
+
+        self.tax = 0
+
+
+    def pass_inspection(self, passed_inspection):
+
+        self.passed_inspection = passed_inspection
+
